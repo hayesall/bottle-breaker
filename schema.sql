ecb8d1edcefd7c05b45d70feaa7cfac3a7eca2a6
hayesall <alexander@batflyer.net>
2023-04-07 23:16:37 -0400

    🗃️ Simplify users/posts table
    I re-read the sqlite docs and simplified the datatypes
    (e.g. to prefer TEXT over VARCHAR since the types seem
    to end up like that anyway).
    
    This also enforces a constraint better, and cascades
    the deletes for 'posts' when a 'user' is deleted.

--- schema.sql
+++ schema.sql
@@ -1,25 +1,26 @@
 -- Copyright © 2023 Alexander L. Hayes
 -- MIT License or Apache 2.0 License, at your choosing
 
-PRAGMA foreign_keys = ON;
 
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS posts;
 
--- A user has a unique id, a username, a hashed password, a salt value, and a time stamp for the last password reset
+-- A user has a unique username, a hashed password, a salt value, and a time stamp for the last password reset
 CREATE TABLE users (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    username VARCHAR(15),
-    password_hash VARCHAR(100),
-    salt VARCHAR(100),
+    username TEXT NOT NULL PRIMARY KEY,
+    password_hash TEXT,
+    salt TEXT,
     last_reset TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Posts are global, associated with a user, and posts are deleted if a user is deleted
 CREATE TABLE posts (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    by_user INTEGER NOT NULL,
+    by_user TEXT NOT NULL,
     post_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     content TEXT NOT NULL,
-    FOREIGN KEY(by_user) REFERENCES users(id) ON UPDATE CASCADE
+    CONSTRAINT user_foreign_key
+        FOREIGN KEY (by_user)
+        REFERENCES users (username)
+        ON DELETE CASCADE
 );
